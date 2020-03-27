07bcaca0d425bb64391690171ae2031a2b752f8a
Tanmay Naik <tnaik96@gmail.com>
2020-03-27 11:27:40 -0400

    added remaining session methods

--- controllers/session.controller.server.js
+++ controllers/session.controller.server.js
@@ -12,13 +12,21 @@ module.exports = (app) => {
         res.send(value);
     }
 
+    function getSessionAll(req, res) {
+        res.send(req.session);
+    }
+    function resetSession(req, res) {
+        req.session.destroy();
+        res.send(200);
+    }
+
     app.get('/api/session/set/:name/:value',
         setSession);
     app.get('/api/session/get/:name',
         getSession);
-    // app.get('/api/session/get',
-    //     getSessionAll);
-    // app.get('/api/session/reset',
-    //     resetSession);
+    app.get('/api/session/get',
+        getSessionAll);
+    app.get('/api/session/reset',
+        resetSession);
 
 }
