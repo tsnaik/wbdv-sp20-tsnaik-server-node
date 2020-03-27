35ac7588cbfe9f2a3ac7f18126e192c33fdfa195
Tanmay Naik <tnaik96@gmail.com>
2020-03-27 10:47:00 -0400

    added body-parser

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/whiteboard-cs5610-sp20',
     { useNewUrlParser: true, useUnifiedTopology: true })
