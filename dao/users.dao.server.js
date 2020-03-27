199bb2c98cc405dfc769ae15f51f9cf14ee63b25
Tanmay Naik <tnaik96@gmail.com>
2020-03-27 10:32:15 -0400

    populated some of the methods in dao

--- dao/users.dao.server.js
+++ dao/users.dao.server.js
@@ -1,7 +1,18 @@
-const createUser = (user) => { }
-const findAllUsers = () => { }
+const userModel = require('../models/users.model.server')
+
+const createUser = (user) => {
+    return userModel.create(user)
+}
+const findAllUsers = () => {
+    return userModel.find()
+}
 const findUserById = (userId) => { }
-const findUserByCredentials = (username, password) => { }
+const findUserByCredentials = (username, password) => {
+    return userModel.findOne({
+        username: username,
+        password: password
+    })
+}
 
 module.exports = {
     createUser, findAllUsers, findUserById, findUserByCredentials
