943a2fb612dd981198582d9c9bb999f1f46229f3
Tanmay Naik <tnaik96@gmail.com>
2020-03-27 11:17:26 -0400

    added session config

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const app = express();
 var bodyParser = require('body-parser')
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
- 
+
 // parse application/json
 app.use(bodyParser.json())
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/whiteboard-cs5610-sp20',
     { useNewUrlParser: true, useUnifiedTopology: true })
 
+var session = require('express-session');
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'the biggest secret the world has ever seen'
+}))
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers',
