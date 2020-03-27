de3a0b58cf74c7a7bfc1de46075692b158240a60
Tanmay Naik <tnaik96@gmail.com>
2020-03-27 00:35:42 -0400

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next();
+});
+
 require('./controllers/quizzes.controller.server')(app);
 require('./controllers/questions.controller.server')(app);
 
