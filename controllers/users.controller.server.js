da4ebd227c5189ec1e339970d08d366e00c23b2c
Tanmay Naik <tnaik96@gmail.com>
2020-03-27 10:41:13 -0400

    added get

--- controllers/users.controller.server.js
+++ controllers/users.controller.server.js
@@ -1,8 +1,14 @@
 const userDao = require('../dao/users.dao.server')
 module.exports = (app) => {
-    app.post('api/users', (req, res) => {
+    app.post('api/users', async (req, res) => {
         const newUser = req.body;
         const actualUser = await userDao.createUser(newUser);
         res.send(actualUser);
+    })
+
+    // not final. just for practice
+    app.get('/api/users', async (req, res) => {
+        const users = await userDao.findAllUsers();
+        res.send(users);
     })
 }
