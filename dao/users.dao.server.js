d1d6bfb2b3911a148d248e93f6f42ec4789bf152
Tanmay Naik <tnaik96@gmail.com>
2020-03-27 10:28:56 -0400

    added user DAO shell

---
+++ dao/users.dao.server.js
@@ -0,0 +1,8 @@
+const createUser = (user) => { }
+const findAllUsers = () => { }
+const findUserById = (userId) => { }
+const findUserByCredentials = (username, password) => { }
+
+module.exports = {
+    createUser, findAllUsers, findUserById, findUserByCredentials
+}
