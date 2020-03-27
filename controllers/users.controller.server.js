4a0082d6941dc80cef2243eaf9fbf19e5386efab
Tanmay Naik <tnaik96@gmail.com>
2020-03-27 10:58:04 -0400

    added login api

--- controllers/users.controller.server.js
+++ controllers/users.controller.server.js
@@ -5,6 +5,22 @@ module.exports = (app) => {
         const actualUser = await userDao.createUser(newUser);
         res.send(actualUser);
     })
+    
+
+
+    app.post('/api/login', async (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+        const user = await userDao.findUserByCredentials(username, password);
+        if(user) {
+            user.password = '**';
+            return res.send(user);
+        } else {
+            return res.status(403).send({
+                message: `User ${username} not found`
+            })
+        }
+    })
 
     // not final. just for practice
     app.get('/api/users', async (req, res) => {
